ac1878594be03a8da868e07d98239b14365d4be9
Damian <damiancb97@gmail.com>
2024-11-21 21:10:34 +0100

    estructura main gestionada

---
+++ js/app.js
@@ -0,0 +1,6 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    hljs.highlightAll();
+  });
+</script>
